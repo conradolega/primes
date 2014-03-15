e8f966034885141efc0d4b0d870d4eca5303820f
Conrad <conradolega@gmail.com>
2014-03-15 11:11:34 +0800

    Add MPI test

---
+++ mpi_test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+
+	// initialize MPI
+	MPI_Init(&argc, &argv);
+
+	// rank = rank of each process
+	// size = number of processes
+	int rank, size;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	printf("Rank: %d Size: %d\n", rank, size);
+
+	// last MPI function to be called
+	MPI_Finalize();
+	return 0;
+}
