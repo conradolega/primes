22e58361e77981002690035b09ad3d4acf7fedca
Conrad <conradolega@gmail.com>
2014-03-15 11:29:24 +0800

    Add message passing test

--- mpi_test.c
+++ mpi_test.c
@@ -9,10 +9,22 @@ int main(int argc, char **argv) {
 
 	// rank = rank of each process
 	// size = number of processes
-	int rank, size;
+	int rank, size, number, i;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	printf("Rank: %d Size: %d\n", rank, size);
+	
+	// sender process
+	if (rank != 0) {
+		MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	}
+	// receiving process
+	else {
+		for (i = 1; i < size; i++) {
+			MPI_Recv(&number, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			printf("Process %d has called\n", number);
+		} 
+	}
 
 	// last MPI function to be called
 	MPI_Finalize();
